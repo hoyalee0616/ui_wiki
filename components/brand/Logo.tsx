12513b6a34e9ffad1f939294c5b241027f9cbda2
맥미니재호 <test@testui-Macmini.lan>
2026-04-25 19:40:42 +0900

    Update image paths to logo.png and logo2.png

--- components/brand/Logo.tsx
+++ components/brand/Logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 function MascotMark() {
   return (
     <img 
-      src="/logo_mascot.png" 
+      src="/logo.png" 
       alt="Logo Mascot" 
       className="logo-mark" 
       style={{ objectFit: "contain" }}
@@ -26,12 +26,8 @@ export function Logo() {
 export function HeroMascot() {
   return (
     <div className="mascot-hero">
-      {/* 
-        채팅창에 올려주신 두 번째 귀여운 곰돌이 이미지를 
-        프로젝트의 public 폴더 안에 'hero_mascot.png' 라는 이름으로 저장해 주세요!
-      */}
       <img 
-        src="/hero_mascot.png" 
+        src="/logo2.png" 
         alt="Gomdol Mascot" 
         style={{ width: "100%", height: "100%", objectFit: "contain" }} 
       />
