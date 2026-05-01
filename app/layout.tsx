b36724f8690c6876286d2667b8b3c6c2e5858abd
맥미니재호 <test@testui-Macmini.lan>
2026-05-01 10:04:48 +0900

    Add GA4 tracking script

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,19 @@ export default function RootLayout({
         {children}
         <Script
           async
+          src="https://www.googletagmanager.com/gtag/js?id=G-ERX87YW610"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ERX87YW610');
+          `}
+        </Script>
+        <Script
+          async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5188089974325767"
           strategy="afterInteractive"
           crossOrigin="anonymous"
